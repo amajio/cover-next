99424f0165d190825554b1e9411cfcb302c403df
amajio <33132275+amajio@users.noreply.github.com>
2024-03-15 14:25:17 +0700

    Update cover-next.user.js

--- cover-next.user.js
+++ cover-next.user.js
@@ -1,28 +1,40 @@
 // ==UserScript==
-// @name         Cover Next (Siambit)
+// @name         Cover Next 2.0(Bearbit)
 // @namespace    http://tampermonkey.net/
-// @version      1.37
+// @version      2.00
 // @description  tools that will help you see new items and make it easier for you to use the website to load bittorrent.
 // @author       NEKO
 // @icon         https://i.imgur.com/qTvfJSr.png
-// @match        https://www.siambit.me
-// @match        https://www.siambit.me/index.php*
-// @match        https://www.siambit.me/viewno18.php*
-// @match        https://www.siambit.me/viewbr.php*
-// @match        https://www.siambit.me/viewno18sb.php*
-// @match        https://www.siambit.me/viewbrsb.php*
-// @match        https://www.siambit.me/torrent_day.php*
-// @match        https://www.siambit.me/torrent_week.php*
-// @match        https://www.siambit.me/torrent_month.php*
-// @match        https://www.siambit.me/upfinish.php*
-// @match        https://www.siambit.me/downfinish.php*
-// @match        https://www.siambit.me/details.php*
+// @match        https://www.bearbit.co
+// @match        https://www.bearbit.co/index.php*
+// @match        https://www.bearbit.co/viewno18.php*
+// @match        https://www.bearbit.co/viewbr.php*
+// @match        https://www.bearbit.co/viewno18sb.php*
+// @match        https://www.bearbit.co/viewbrsb.php*
+// @match        https://www.bearbit.co/torrent_day.php*
+// @match        https://www.bearbit.co/torrent_week.php*
+// @match        https://www.bearbit.co/torrent_month.php*
+// @match        https://www.bearbit.co/upfinish.php*
+// @match        https://www.bearbit.co/downfinish.php*
+// @match        https://www.bearbit.co/details.php*
+// @match        https://bearbit.co
+// @match        https://bearbit.co/index.php*
+// @match        https://bearbit.co/viewno18.php*
+// @match        https://bearbit.co/viewbr.php*
+// @match        https://bearbit.co/viewno18sb.php*
+// @match        https://bearbit.co/viewbrsb.php*
+// @match        https://bearbit.co/torrent_day.php*
+// @match        https://bearbit.co/torrent_week.php*
+// @match        https://bearbit.co/torrent_month.php*
+// @match        https://bearbit.co/upfinish.php*
+// @match        https://bearbit.co/downfinish.php*
+// @match        https://bearbit.co/details.php*
 // @grant        GM_addStyle
 // @grant        GM_xmlhttpRequest
-// @require      https://raw.githubusercontent.com/kon3ko/cover-next/master/dist/cover-next.js?version=1.37
-// @updateURL    https://raw.githubusercontent.com/kon3ko/cover-next/master/cover-next.user.js
-// @downloadURL  https://raw.githubusercontent.com/kon3ko/cover-next/master/cover-next.user.js
-// @supportURL   https://github.com/kon3ko/cover-next/issues
+// @require      https://raw.githubusercontent.com/amajio/cover-next/master/dist/cover-next.js?version=2.00
+// @updateURL    https://raw.githubusercontent.com/amajio/cover-next/master/cover-next.user.js
+// @downloadURL  https://raw.githubusercontent.com/amajio/cover-next/master/cover-next.user.js
+// @supportURL   https://github.com/amajio/cover-next/issues
 // @connect      img.in.th
 // @connect      bpicc.cc
 // @connect      imgtrue.com
@@ -65,5 +77,9 @@
 // @connect      uppic.cc
 // @connect      uppicimg.com
 // @connect      uppic.xyz
+// @connect      gifyu.com
+// @connect      imgvb.com
+// @connect      up-pic.com
+// @connect      pic.4th.in
 // @connect      *
 // ==/UserScript==
